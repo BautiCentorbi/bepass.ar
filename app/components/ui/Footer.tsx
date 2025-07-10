ceadb252623c14acc48200a96b4d05ab305c089f
Bautista Centorbi <bautii.centorbii@gmail.com>
2025-07-10 00:04:23 -0300

    Smooth Scroll Fix 1.1

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col md:flex-row justify-center items-center py-8 md:py-12'>
+      <Image src="/images/MUTA-Logo_Footer.svg" alt="Logo de MUTA" width={214} height={120} />
+    </footer>
   )
 }
 
